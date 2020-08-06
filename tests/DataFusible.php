cec1efbd50f7c4e8855791d71b88eb961869e8bb
Facu <subiabrewd@gmail.com>
2020-08-06 19:01:09 +0200

    Update data fusible

--- tests/DataFusible.php
+++ tests/DataFusible.php
@@ -9,11 +9,11 @@ class DataFusible extends AbstractFusible
 {
     protected function getRules(Validator $validator): Validator   
     {
-        //$validator->attribute('name', $validator->notBlank());
-        //$validator->attribute('password', $validator->notBlank());
-        //$validator->attribute('number', $validator->number());
-        $validator->attribute('blank', $validator->notBlank())
-            ;
+        $validator->allOf(
+            $validator::attribute('name', $validator::notBlank()),
+            $validator::attribute('password', $validator::notBlank()),
+            $validator::attribute('number', $validator::number())
+        );
 
         return $validator;
     }
